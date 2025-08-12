27e21eb3ed83ee9608fdad55208131ce78ee7664
vados-pro <muravyov.official@gmail.com>
2025-08-12 12:58:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'white';
+ctx.font = '30px Arial';
+ctx.fillText('Здесь будет твоя игра', 200, 300);
